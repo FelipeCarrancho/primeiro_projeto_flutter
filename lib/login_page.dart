6888a9eb8ac32b82e2be04b379292ee2ca5f28f5
Felipe Carrancho <f.carrancho@hotmail.com>
2025-06-03 12:10:35 -0300

    adicionado background

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,63 +11,79 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Container(height: 20),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (text) {
+                  password = text;
+                },
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  if (email == 'felipe@teste.com.br' && password == '1234') {
+                    // meramente criado para teste
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    debugPrint('login invalido');
+                  } //O print() é considerado inadequado para código de produção
+                },
+                child: const Text('Entrar'),
+              ), // RaisedButton virou ElebatedButton
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 200,
-                  height: 200,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                Container(height: 20),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'felipe@teste.com.br' && password == '1234') {
-                      // meramente criado para teste
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      debugPrint('login invalido');
-                    } //O print() é considerado inadequado para código de produção
-                  },
-                  child: const Text('Entrar'),
-                ), // RaisedButton virou ElebatedButton
-              ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/DSC_0156-copiar2-scaled.webp',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+          Container(color: Colors.black.withValues(alpha: 0.4)),
+          _body(),
+        ],
       ),
     );
   }
